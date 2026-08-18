5dc7f43a8f71a4c3822181613ee196de16f7e5c4
Randrianarison Eddou <eddourandria@gmail.com>
2026-08-18 14:13:42 +0300

    Add routing configuration for MonPremierComposant

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MonPremierComposant } from './mon-premier-composant/mon-premier-composant';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Route par défaut (quand l'URL est vide)
+  { path: '', redirectTo: 'produit', pathMatch: 'full' },
+
+  // Route vers notre composant
+  { path: 'produit', component: MonPremierComposant},
+
+];
